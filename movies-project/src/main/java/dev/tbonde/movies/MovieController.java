fa7d6f0378d3acd5da1bea22ed5e2a7fd198d3ad
tomburger64 <tom64bonde@gmail.com>
2025-02-27 15:28:26 +0100

    http://localhost:8080/api/v1/movies returns something (I don't get too much of what I'm doing, I'll definitely do more apps afterwards)

---
+++ movies-project/src/main/java/dev/tbonde/movies/MovieController.java
@@ -0,0 +1,14 @@
+package dev.tbonde.movies;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/movies")
+public class MovieController {
+    @GetMapping
+    public String allMovies() {
+        return "All movies!";
+    }
+}
